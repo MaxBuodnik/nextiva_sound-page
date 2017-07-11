18d6c4a0f1d1eb36be2821321312aed55776ecae
Maksym Boudnik <maksym.boudnik@nextiva.com>
2017-07-11 19:12:14 +0300

    Added display of the list of tracks and video lings

--- src/Components/AlbumView.js
+++ src/Components/AlbumView.js
@@ -5,16 +5,24 @@ import {store} from '../Store/Store';
 class ChosenAlbum extends Component {
 
   render() {
+    console.log(this.props.tracks, 'props')
     return (
       <div className='shosenAlbumView'>
-        <h4 style={{textAlign: 'center'}}>{this.props.author} - {this.props.album}</h4>
+        <h2 style={{textAlign: 'center'}}>{this.props.author} - {this.props.album}</h2>
         <img src={this.props.image} alt={this.props.author}/>
         <ul>
           <li>Label: {this.props.label}</li>
           <li>Genre: {this.props.genre}</li>
           <li>Year of manufacture: {this.props.year}</li>
           <li>Format: {this.props.format}</li>
+          <p>Tracks list:</p>
+          <ul>
+            {this.props.tracks.map((track) => {
+              return <li key={track}>{track}</li>
+            })}
+          </ul>
         </ul>
+        <a href={this.props.video}>Video on youtube</a>
       </div>
     )
   }
@@ -24,10 +32,10 @@ export default class AlbumView extends Component {
   render() {
     const chosenAlbum = store.getState();
     const showAlbum = (
-      chosenAlbum.albumsStore.map(([{Author, Album, Image, Label, Genre, Year, Format, id}]) => (
+      chosenAlbum.albumsStore.map(([{Author, Album, Image, Label, Genre, Year, Format, id, Tracks, Video}]) => (
         <ChosenAlbum key={`item-${id}`} author={Author} album={Album} image={Image} label={Label} genre={Genre}
                      year={Year}
-                     format={Format} id={id}/>
+                     format={Format} id={id} tracks={Tracks} video={Video} />
       ))
     );
     return (
